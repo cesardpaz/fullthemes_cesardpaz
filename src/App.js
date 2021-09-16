79026d1756023a7db051b5204f363f5371f1ef11
cesardpaz <cesardpaz@gmail.com>
2021-09-16 07:07:27 -0500

    version 0.1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './template_logo_512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Fullthemes</h1>
+        <p>Get the best web templates</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
