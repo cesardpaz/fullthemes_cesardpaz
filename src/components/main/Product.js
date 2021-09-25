eb563f0f25b0cb15ab291ae7e921d3a8e714240f
cesardpaz <cesardpaz@gmail.com>
2021-09-25 08:31:23 -0500

    price format

--- src/components/main/Product.js
+++ src/components/main/Product.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Product = ({product}) => {
 
-    const {title, author, cover, price} = product; 
+    const {title, author, cover, price} = product;
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
+
     return (
         <div className="main__wdgt__card">
             <figure>
@@ -11,7 +18,7 @@ const Product = ({product}) => {
             <div className="wdgt__card__item">
                 <h2>{title}</h2>
                 <h3>By <span>{author}</span></h3>
-                <h4>${price}</h4>
+                <h4>{formatter.format(price)}</h4>
                 <button>Add to cart</button>
             </div>
         </div>
